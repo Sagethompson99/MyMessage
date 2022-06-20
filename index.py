e62bcf7a5d026a3bf7d86ab699c327818695671e
Sage Thompson <sagethompson85@gmail.com>
2022-06-20 02:17:55 -0400

    added functionality

--- index.py
+++ index.py
@@ -1,22 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hslkeurygw3i47wpyio7rygfserityw734tw6o83476tweri85858yrfgskufyvgshfgeri8'
 bootstrap = Bootstrap(app)
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
+# @app.errorhandler(404)
+# def page_not_found(e):
+#    return render_template('404.html'), 404
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
 @app.route('/marilyn', methods=['GET', 'POST'])
-def success():
-    return render_template("marilyn.html")
+def marilyn():
+    if request.method == 'POST':
+        request_data = request.get_json()
+        file = open("messages_for_marilyn.txt", "a")  # append mode
+        file.write(request_data["message"])
+        file.close()
+        return render_template("marilyn.html")
+    else:
+        file = open("messages_for_sage.txt", "r")
+        return file.readline()
 
 @app.route('/sage', methods=['GET', 'POST'])
-def success():
-    return render_template("sage.html")
+def sage():
+    if request.method == 'POST':
+        request_data = request.get_json()
+        file = open("messages_for_sage.txt", "a")  # append mode
+        file.write(request_data["message"])
+        file.close()
+        return render_template("sage.html")
+    else:
+        file = open("messages_for_sage.txt", "r")
+        return file.readline()
